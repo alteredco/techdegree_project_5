91d19f14bfd39911a3491ea343725c9827a157d6
Wendy <alteredco@gmail.com>
2019-08-18 23:05:58 +0200

    add modal start

--- js/index.js
+++ js/index.js
@@ -48,21 +48,27 @@ async function getRandUsers(url) {
 
 function searchFilter() {
   let searchInput = document.getElementById('search-input');
+  let searchBtn = document.getElementById('search-submit');
   searchInput.addEventListener('keyup', (e) => {
     let nameInput =searchInput.value.toLowerCase();
     filterNames(nameInput);
   });
-  function filterNames(input) {
-    let names = document.querySelectorAll('.card-name');
-    names.forEach(name => {
-      genName = name.innerText.toLowerCase();
-      if(genName.match(input)) {
-        name.parentNode.parentNode.style.display= "block";
-      } else {
-        name.parentNode.parentNode.style.display="none";
-      }
-    });
-  }
+  searchBtn.addEventListener('click', (e)=> {
+    let nameInput =searchInput.value.toLowerCase();
+    filterNames(nameInput);
+  });
+}
+
+function filterNames(input) {
+  let names = document.querySelectorAll('.card-name');
+  names.forEach(name => {
+    genName = name.innerText.toLowerCase();
+    if(genName.match(input)) {
+      name.parentNode.parentNode.style.display= "block";
+    } else {
+      name.parentNode.parentNode.style.display="none";
+    }
+  });
 }
 
 // Generate the markup for each profile
@@ -70,7 +76,7 @@ function generateHTML(data) {
   searchContainer.innerHTML = `
   <form action="#" method="get">
     <input type="search" id="search-input" class="search-input" placeholder="Search...">
-    <input type="submit" value="&#x1F50D;" id="serach-submit" class="search-submit">
+    <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
   </form>`;
   data.map(person => {
     const section = document.createElement('section');
@@ -97,11 +103,38 @@ function modalHandler() {
   }));
 }
 
+function modalGenerator(data) {
+  data.map(person=>{
+   const modalDiv = document.createElement('div');
+   modalDiv.innerHTML = `
+   <div class="modal-container">
+                <div class="modal">
+                    <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                    <div class="modal-info-container">
+                        <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
+                        <h3 id="name" class="modal-name cap">name</h3>
+                        <p class="modal-text">email</p>
+                        <p class="modal-text cap">city</p>
+                        <hr>
+                        <p class="modal-text">(555) 555-5555</p>
+                        <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
+                        <p class="modal-text">Birthday: 10/21/2015</p>
+                    </div>
+                </div>
+                <div class="modal-btn-container">
+                    <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+                    <button type="button" id="modal-next" class="modal-next btn">Next</button>
+                </div>
+   `
+  } )
+}
+
 // ------------------------------------------
 
 getRandUsers(randGroupUrl)
   .then(generateHTML)
   .then(searchFilter)
+  .then(modalGenerator)
   .catch(err => {
     gallery.innerHTML = '<h3>Something went wrong...</h3>';
     console.error(err);
