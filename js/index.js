5cb4ec2a57b1680ebfdb9f260b7bfd44aca08fad
Wendy <alteredco@gmail.com>
2019-08-19 07:44:56 +0200

    add modals

--- js/index.js
+++ js/index.js
@@ -30,11 +30,17 @@ async function getRandUsers(url) {
       let name = `${person.name.first} ${person.name.last}`;
       let email = person.email;
       let location = `${person.location.city}, ${person.location.state}`;
+      let phone = person.cell;
+      let address = `${person.location.street}, ${person.location.state}, ${person.location.postcode}`;
+      let dob = person.dob.date;
       return {
         image,
         name,
         email,
-        location
+        location,
+        phone,
+        address,
+        dob
       };
     }));
     return profiles;
@@ -63,16 +69,13 @@ function filterNames(input) {
   let names = document.querySelectorAll('.card-name');
   names.forEach(name => {
     genName = name.innerText.toLowerCase();
-    if(genName.match(input)) {
-      name.parentNode.parentNode.style.display= "block";
-    } else {
-      name.parentNode.parentNode.style.display="none";
-    }
+    genName.match(input) ? name.parentNode.parentNode.style.display= "block" : name.parentNode.parentNode.style.display="none";
   });
 }
 
 // Generate the markup for each profile
 function generateHTML(data) {
+  const num = 0;
   searchContainer.innerHTML = `
   <form action="#" method="get">
     <input type="search" id="search-input" class="search-input" placeholder="Search...">
@@ -93,48 +96,85 @@ function generateHTML(data) {
         </div>
       </div>
       `;
+      const modalDiv = document.createElement('div');
+      modalDiv.className +="modal-container"
+      gallery.appendChild(modalDiv);
+      modalDiv.innerHTML = `
+          <div class="modal" id=${person.dob}>
+            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+             <div class="modal-info-container">
+                              <img class="modal-img" src=${person.image} alt=${person.name}>
+                              <h3 id="name" class="modal-name cap">${person.name}</h3>
+                              <p class="modal-text">${person.email}</p>
+                              <p class="modal-text cap">${person.location}</p>
+                              <hr>
+                              <p class="modal-text">${person.phone}</p>
+                              <p class="modal-text">${person.address}</p>
+                              <p class="modal-text">Birthday: ${person.dob}</p>
+              </div>
+             </div>
+                      <div class="modal-btn-container">
+                          <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+                          <button type="button" id="modal-next" class="modal-next btn">Next</button>
+                      </div>
+        `;
+        modalDiv.style.display="none";
       });
 }
 
 function modalHandler() {
-  const cards = document.getElementsByClassName('cards');
-  cards.forEach(card => card.addEventListener('click', (e)=> {
-    console.log('working');
-  }));
+  document.querySelectorAll('.card').forEach(card => {
+    card.addEventListener('click', e => {
+      const currCard = e.currentTarget;
+      const currName = currCard.querySelector('.card-name').innerText.toLowerCase();
+      const modals = document.querySelectorAll('.modal-container');
+      modals.forEach(modal => {
+        const modalName = modal.querySelector('#name').innerText
+        currName===modalName ? modal.style.display='block'  : modal.style.display = 'none';
+      });
+    });
+  });
+  const closeModalBtn = document.getElementById('modal-close-btn');
+  closeModalBtn.addEventListener('click', e => {
+    console.log('close')
+  })
 }
 
-function modalGenerator(data) {
-  data.map(person=>{
-   const modalDiv = document.createElement('div');
-   modalDiv.innerHTML = `
-   <div class="modal-container">
-                <div class="modal">
-                    <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-                    <div class="modal-info-container">
-                        <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
-                        <h3 id="name" class="modal-name cap">name</h3>
-                        <p class="modal-text">email</p>
-                        <p class="modal-text cap">city</p>
-                        <hr>
-                        <p class="modal-text">(555) 555-5555</p>
-                        <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
-                        <p class="modal-text">Birthday: 10/21/2015</p>
-                    </div>
-                </div>
-                <div class="modal-btn-container">
-                    <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
-                    <button type="button" id="modal-next" class="modal-next btn">Next</button>
-                </div>
-   `
-  } )
-}
+// function modalGenerator() {
+//    const modalDiv = document.createElement('div');
+//    gallery.appendChild(modalDiv);
+//    modalDiv.innerHTML = `
+//    <div class="modal-container">
+//                 <div class="modal">
+//                     <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+//                     <div class="modal-info-container">
+//                         <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
+//                         <h3 id="name" class="modal-name cap">name</h3>
+//                         <p class="modal-text">email</p>
+//                         <p class="modal-text cap">city</p>
+//                         <hr>
+//                         <p class="modal-text">(555) 555-5555</p>
+//                         <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
+//                         <p class="modal-text">Birthday: 10/21/2015</p>
+//                     </div>
+//                 </div>
+//                 <div class="modal-btn-container">
+//                     <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+//                     <button type="button" id="modal-next" class="modal-next btn">Next</button>
+//                 </div>
+//    `;
+   
+
+// }
 
 // ------------------------------------------
 
 getRandUsers(randGroupUrl)
   .then(generateHTML)
   .then(searchFilter)
+  .then(modalHandler)
   .catch(err => {
     gallery.innerHTML = '<h3>Something went wrong...</h3>';
     console.error(err);
   });
+
