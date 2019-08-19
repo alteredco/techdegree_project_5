75a7595e20fe42209c004f3b750fc2e666cce51d
Wendy <alteredco@gmail.com>
2019-08-19 15:17:33 +0200

    add style and lego

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 
 const randUserUrl = 'https://randomuser.me/api/';
 const groupNum = "12"
-const randGroupUrl = randUserUrl+`/?nat=au,us,dk,fr,gb&results=${groupNum}`;
+const randGroupUrl = randUserUrl+`/?nat=au,us,dk,fr,gb&lego&results=${groupNum}`;
 
 const searchContainer = document.querySelector('.search-container');
 const gallery = document.getElementById('gallery');
